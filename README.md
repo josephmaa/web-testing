b5ca2e639de7ef3151cbbb1bca23bf2e788027e4
Joseph Maa <josephgmaa@berkeley.edu>
2022-10-24 14:32:12 -0700

    Add firefox

--- README.md
+++ README.md
@@ -5,19 +5,28 @@ Runs on the following [website](http://automationpractice.com/index.php).
 
 ![](imgs/main.png)
 
+Runs on both chrome and firefox browsers to test browser compatibility.
+
 To run the tests:
 
-1. Run the command in order to create the conda environment:
+1. Run the command in order to create the conda environment.
+
 `conda create -n web-testing python=3.10`
 
-2. Run the command to install pip
+2. Run the command to install pip.
+
 `conda install pip`
 
-3. Run the command to install the required commands
+3. Run the command to install the required commands.
+
 `pip install -r requirements.txt`
 
 4. Run the command to install the local module to the conda environment.
+
 `pip install -e .`
 
-5. Run the tests with the command
+5. Run the tests with the command.
+
 `python3 -m pytest`
+
+Pytest can be run with the `-s` flag in order to print output and the `-k` flag to run single tests at a time.
---
+++ tests/test_firefox.py
@@ -0,0 +1,53 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+import unittest
+import configparser
+from typing import Optional
+
+
+class test_website_firefox(unittest.TestCase):
+    def setUp(self):
+        parser = configparser.ConfigParser()
+        parser.read("web.ini")
+        self.driver: Optional["webdriver"] = None
+        firefox_service = Service("./drivers/geckodriver")
+        self.driver = webdriver.Firefox(service=firefox_service)
+        self.driver.get(parser["DEFAULT"]["url"])
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_ajax_block_element_0(self):
+        elements = self.driver.find_elements(By.CSS_SELECTOR, "li.ajax_block_product")
+        self.driver.implicitly_wait(1)
+        if elements[0].is_displayed():
+            self.driver.implicitly_wait(1)
+            elements[0].click()
+            self.driver.implicitly_wait(1)
+            self.driver.back()
+
+    def test_ajax_block_element_5(self):
+        elements = self.driver.find_elements(By.CSS_SELECTOR, "li.ajax_block_product")
+        self.driver.implicitly_wait(1)
+        if elements[5].is_displayed():
+            self.driver.implicitly_wait(1)
+            elements[5].click()
+            self.driver.implicitly_wait(1)
+            self.driver.back()
+
+    def test_input_box_enter(self):
+        element = self.driver.find_element(By.CSS_SELECTOR, "input.search_query")
+        element.send_keys("Testing")
+        element.send_keys(Keys.ENTER)
+
+    def test_input_box_submit(self):
+        element = self.driver.find_element(By.CSS_SELECTOR, "input.search_query")
+        element.send_keys("Testing")
+        element.submit()
+
+
+if __name__ == "__main__":
+    unittest.main()
